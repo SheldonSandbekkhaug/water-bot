7cec5e6b50b935b2ae4d9da8934f36f9cf35b69c
Sheldon S <sheldon@sandbekkhaug.net>
2016-12-10 17:02:08 -0800

    Create markov_tweet_source Add some books for markov bot

---
+++ src/markov_tweet_source.py
@@ -0,0 +1,37 @@
+import os
+
+from markovbot import MarkovBot
+
+# Directory for tweet content.
+content_dir = 'content'
+
+def markov_tweet():
+    """ Generates a tweet using a Markov bot. """
+    bot = MarkovBot()
+    read_book(bot, 'alice.txt')
+    read_book(bot, 'dao.txt')
+    read_book(bot, 'oz.txt')
+    
+    seed_words = ['drink', 'health', 'water', 'river', 'stream']
+    tweet_content = bot.generate_text(15, seedword=seed_words)
+    print tweet_content
+    
+    # Check if the generated content has a seed word in it.
+    for word in seed_words:
+        if word in tweet_content:
+            return tweet_content + ' #DrinkWater'
+    return None
+        
+def read_book(bot, filename):
+    """ Tells the Markov bot to read the book with the given file name. """
+    filepath = get_filepath(filename)
+    bot.read(filepath)    
+    return
+    
+# TODO: Refactor I/O code into its own file.
+def get_filepath(filename):
+    """ Returns the filepath in the content directory with the given filename.
+    """
+    content_source_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    content_filepath = os.path.join(content_source_dir, content_dir, filename)
+    return content_filepath    
--- src/water_bot.py
+++ src/water_bot.py
@@ -2,6 +2,8 @@
 
 from secrets import *
 
+import os
+import markov_tweet_source
 import simple_tweet_source
 import tweepy
 
@@ -12,9 +14,14 @@ def main():
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
     
-    # Get some content to tweet.
+    tweet_content = markov_tweet_source.markov_tweet()
+    if tweet_content and len(tweet_content) <= 140:
+        api.update_status(tweet_content)
+        print 'Tweeted: ' + tweet_content
+    else:
+        print 'Nothing tweeted.'
+    
+def simple_tweet():
     tweet_content = simple_tweet_source.new_tweet()
-    api.update_status(tweet_content)
-    print 'Tweeted: ' + tweet_content
     
 main()
